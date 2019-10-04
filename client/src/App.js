61d867e5d5f4e72b06b1fc5534dbfd99ebf57812
Mark Gowen <mark.gowen78@gmail.com>
2019-10-04 08:25:32 -0700

    Initial Setup
    Added dependencies and Material UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+// Material UI
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+// Styles
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+}));
+
+
+
 function App() {
+const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className={classes.root}>
+    <AppBar position="static">
+      <Toolbar variant="dense">
+        <Typography variant="h6" color="inherit">
+          Women's World Cup
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  </div>
   );
 }
 
